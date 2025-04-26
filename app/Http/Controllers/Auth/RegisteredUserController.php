ec01048d0f595f070f4ccd48fb464454586b703a
khaleddoosama <khaleddoosama@gmail.com>
2025-04-26 14:00:02 +0200

    02-02 Customize Registeration Page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,12 +31,18 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username' =>['required','string','max:30'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        //https://ui-avatars.com/api/?name=John+Doe
+        // Mohamed+Osama
+        
         $user = User::create([
             'name' => $request->name,
+            'image'=> 'https://ui-avatars.com/api/?name='. urlencode($request->name),
+            'username'=>$request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -45,6 +51,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('welcome'));
     }
 }
